d5cbf34e9460a27b53671db1e4e35b71f2b6eeba
Arbaz Shoukat <arbazkamboh342@.com>
2024-07-23 15:42:31 +0500

    making some changes

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ app.set("trust proxy", 1);
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [process.env.FRONTEND_ORIGIN],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD"],
     credentials: true,
   }),
