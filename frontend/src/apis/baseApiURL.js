69416103678ef2643026ceeb64080feeaceb3ffa
Arbaz Shoukat <arbazkamboh342@.com>
2024-07-27 16:51:31 +0500

    image url changes

--- frontend/src/apis/baseApiURL.js
+++ frontend/src/apis/baseApiURL.js
@@ -7,7 +7,7 @@ const api = axios.create({
   baseURL: "https://natoures.onrender.com/api/v1", // replace with your base URL
 });
 
-const toursImages = "https://natoures.onrender.com/public/img/tours/";
-const usersImages = "https://natoures.onrender.com/public/img/users/";
+const toursImages = "https://natoures.onrender.com/public/img/tours";
+const usersImages = "https://natoures.onrender.com/public/img/users";
 
 export { api, toursImages, usersImages };
